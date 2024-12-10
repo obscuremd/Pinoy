761460388be6ea54f0c35079d557d79c0dd5e759
dan <md.erhenede@gmail.com>
2024-12-10 15:34:17 +0300

    added fetch all users

--- Server/src/Routes/ReviewsRoute.ts
+++ Server/src/Routes/ReviewsRoute.ts
@@ -1,81 +1,89 @@
 import { Router } from "express";
 import Review from "../Models/ReviewsModel";
-const router = Router()
+const router = Router();
 
 // start Trip
-router.post('/start',async(req, res)=>{
-    const {driver_id, passenger_id, activity, start_location, destination, price, total_time} = req.body
-    try {
-        const newTrip = new Review({driver_id, passenger_id, activity, start_location, destination, price, total_time})
-        await newTrip.save()
-        res.status(200).json(newTrip)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+router.post("/start", async (req, res) => {
+  const {
+    driver_id,
+    passenger_id,
+    activity,
+    start_location,
+    destination,
+    price,
+    total_time,
+  } = req.body;
+  try {
+    const newTrip = new Review({
+      driver_id,
+      passenger_id,
+      activity,
+      start_location,
+      destination,
+      price,
+      total_time,
+    });
+    await newTrip.save();
+    res.status(200).json(newTrip);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // update Trip
-router.put('/:id',async(req, res)=>{
-    
-    try {
-        const trip = await Review.findByIdAndUpdate(req.params.id)
-        
-        if(!trip){
-            res.status(404).json('Trip not found')
-        }else{
-            const newTrip = await trip.updateOne({$set:req.body})
-            res.status(200).json(newTrip)
-        }
-        
-    } catch (error) {
-        res.status(500).json(error)
+router.put("/:id", async (req, res) => {
+  try {
+    const trip = await Review.findByIdAndUpdate(req.params.id);
+
+    if (!trip) {
+      res.status(404).json("Trip not found");
+    } else {
+      const newTrip = await trip.updateOne({ $set: req.body });
+      res.status(200).json(newTrip);
     }
-})
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // get Trip
-router.get('/:id',async(req, res)=>{
-    
-    try {
-        const trip = await Review.findById(req.params.id)
-        if(!trip){
-            res.status(404).json('trip not found')
-        }else{
-            res.status(200).json(trip)
-        }
-        
-    } catch (error) {
-        res.status(500).json(error)
+router.get("/:id", async (req, res) => {
+  try {
+    const trip = await Review.findById(req.params.id);
+    if (!trip) {
+      res.status(404).json("trip not found");
+    } else {
+      res.status(200).json(trip);
     }
-})
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 // get Trip driverId
-router.get('/driver/:id',async(req, res)=>{
-    
-    try {
-        const trip = await Review.findOne({driver_id:req.params.id})
-        if(!trip){
-            res.status(404).json('trip not found')
-        }else{
-            res.status(200).json(trip)
-        }
-        
-    } catch (error) {
-        res.status(500).json(error)
+router.get("/driver/:id", async (req, res) => {
+  try {
+    const trip = await Review.findOne({ driver_id: req.params.id });
+    if (!trip) {
+      res.status(404).json("trip not found");
+    } else {
+      res.status(200).json(trip);
     }
-})
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 // get Trip passengerId
-router.get('/passenger/:id',async(req, res)=>{
-    
-    try {
-        const trip = await Review.findOne({passenger_id:req.params.id})
-        if(!trip){
-            res.status(404).json('trip not found')
-        }else{
-            res.status(200).json(trip)
-        }
-        
-    } catch (error) {
-        res.status(500).json(error)
+router.get("/passenger/:id", async (req, res) => {
+  try {
+    const trip = await Review.findOne({ passenger_id: req.params.id });
+    if (!trip) {
+      res.status(404).json("trip not found");
+    } else {
+      res.status(200).json(trip);
     }
-})
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-export default router
+export default router;
--- Server/src/Routes/UserRoute.ts
+++ Server/src/Routes/UserRoute.ts
@@ -1,75 +1,110 @@
 import { Router } from "express";
 import User from "../Models/UserModel";
-const router = Router()
+const router = Router();
 
-// create user
-router.post('/register', async (req, res) => {
-    const { username, email, phone_number, admin, admin_data_id, driver, drivers_license, car_picture, car_number, activity, status, trips, location} = req.body
-    try {
-        const newUser = new User({ username, email, phone_number, admin, admin_data_id, driver, drivers_license, car_picture, car_number, activity, status, trips: trips, location: location });
-        await newUser.save();
-
-        res.status(200).json(newUser);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-})
-
-// find by id
-router.get('/:id', async (req, res) => {
-    const id = req.params.id
-    try {
-        const user = await User.findById(id);
-
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-})
-
-// find by email
-router.get('/email/:email', async (req, res) => {
-    try {
-        const user = await User.findOne({email: req.params.email});
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-})
-
-// update user
-router.put('/:id', async(req, res) => {
-    try {
-            const user = await User.findById(req.params.id)
-
-                if(!user){
-                    res.status(404).json('no such user found')
-                }else{
-                    const updatedUser = await user?.updateOne({$set:(req.body)})
-                    res.status(200).json(updatedUser)
-                }
-        
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
-
-// delete user
-router.delete('/:id', async (req, res) => {
-    try {
-
-        const user = await User.findById(req.params.id)
-
-        if(!user){
-            res.status(404).json('no such user found')
-        }else{
-            await User.findByIdAndDelete(req.params.id);
-            res.status(200).json({ message: `user has been deleted` });
-        }
-
-    } catch (error) {
-        res.status(500).json({ message: error });
-    }
+// get all users
+router.get("/", async (req, res) => {
+  try {
+    const user = await User.find();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-export default router
+// create user
+router.post("/register", async (req, res) => {
+  const {
+    username,
+    email,
+    phone_number,
+    admin,
+    admin_data_id,
+    driver,
+    drivers_license,
+    car_picture,
+    car_number,
+    activity,
+    status,
+    trips,
+    location,
+  } = req.body;
+  try {
+    const newUser = new User({
+      username,
+      email,
+      phone_number,
+      admin,
+      admin_data_id,
+      driver,
+      drivers_license,
+      car_picture,
+      car_number,
+      activity,
+      status,
+      trips: trips,
+      location: location,
+    });
+    await newUser.save();
+
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// find by id
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await User.findById(id);
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// find by email
+router.get("/email/:email", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.params.email });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// update user
+router.put("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      res.status(404).json("no such user found");
+    } else {
+      const updatedUser = await user?.updateOne({ $set: req.body });
+      res.status(200).json(updatedUser);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// delete user
+router.delete("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      res.status(404).json("no such user found");
+    } else {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json({ message: `user has been deleted` });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+export default router;
