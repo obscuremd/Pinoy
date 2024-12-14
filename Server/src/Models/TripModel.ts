d2bd01be622e81dccd7ce92916de3113695b89f3
dan <md.erhenede@gmail.com>
2024-12-14 19:51:42 +0100

    updated data type

--- Server/src/Models/TripModel.ts
+++ Server/src/Models/TripModel.ts
@@ -1,14 +1,17 @@
 import mongoose, { models } from "mongoose";
 
-const tripSchema = new mongoose.Schema({
-    driver_id: { type: String},
-    passenger_id: { type: String},
+const tripSchema = new mongoose.Schema(
+  {
+    driver_id: { type: String },
+    passenger_id: { type: String },
     activity: { type: Boolean, default: false },
-    start_location: { type: {lng :{type:Number}, lat:{type:Number}}, default: null},
-    destination: { type: {lng :{type:Number}, lat:{type:Number}}, default: null},
-    price:{type:String},
-    total_time: { type: String, default:'--:--'}
-}, {timestamps:true});
+    start_location: { type: [Number, Number], default: [] },
+    destination: { type: [Number, Number], default: [] },
+    price: { type: String },
+    total_time: { type: String, default: "--:--" },
+  },
+  { timestamps: true }
+);
 
-const Trip = models.tripData || mongoose.model('tripData', tripSchema);
+const Trip = models.tripData || mongoose.model("tripData", tripSchema);
 export default Trip;
